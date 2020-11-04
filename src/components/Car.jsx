767178111033ae8ee5571549f407c6fc957ded7a
Rauf Erk <erk.rauf@gmail.com>
2020-11-04 17:54:00 +0300

    react and style test and trainung

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -2,13 +2,24 @@
 
 const сar = ({ key, name, year, color, setNewName }) => {
   // const [inputText, setInputText] = useState('');
+  const inputClasses = ["input"];
+  if (name === "") {
+    inputClasses.push("red");
+  } else {
+    inputClasses.push("green");
+    if (name.length>4){
+      inputClasses.push("bold");
+    }
+  }
   return (
     <div className="carClass" key={key}>
       <h1>{name}</h1>
       <div>year : {year}</div>
       <div>color : {color}</div>
       {/* <button onClick={setTitle.bind(this, name + year + color)}>CLICK</button> */}
+      <div>{inputClasses.join(' ')}</div>
       <input
+        className={inputClasses.join(' ')}
         type="text"
         onChange={(e) => setNewName(e.target.value)}
         value={name}
